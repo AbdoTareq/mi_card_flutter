31aad28715aa88fb5c17438fb6207cb4a151d254
Abdo Tareq <abdotarekfathy@gmail.com>
2020-05-22 15:10:08 +0200

    feat: Migrate to use card with listTitle

--- lib/main.dart
+++ lib/main.dart
@@ -14,33 +14,72 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-          child: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                CircleAvatar(
-                  radius: 50,
-                  backgroundImage: AssetImage('images/im.png'),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              CircleAvatar(
+                radius: 60,
+                backgroundImage: AssetImage('images/d.png'),
+              ),
+              Text(
+                "Abdo Tareq",
+                style: TextStyle(
+                    fontFamily: 'Pacifico',
+                    fontSize: 40,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold),
+              ),
+              Text(
+                "Android developer",
+                style: TextStyle(
+                    fontFamily: 'Source Sans Pro',
+                    fontSize: 20,
+                    color: Colors.teal[100],
+                    letterSpacing: 5,
+                    fontWeight: FontWeight.bold),
+              ),
+              SizedBox(
+                height: 30,
+                width: 150,
+                child: Divider(
+                  color: Colors.white,
                 ),
-                Text(
-                  "Abdo Tareq",
-                  style: TextStyle(
-                      fontFamily: 'Pacifico',
-                      fontSize: 40,
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                    color: Colors.teal,
+                  ),
+                  title: Text(
+                    '0106 1420 874',
+                    style: TextStyle(
+                        fontFamily: 'Source SansPro',
+                        fontSize: 20,
+                        color: Colors.teal[600],
+                        fontWeight: FontWeight.bold),
+                  ),
                 ),
-                Text(
-                  "Android developer",
-                  style: TextStyle(
-                      fontFamily: 'Source Sans Pro',
-                      fontSize: 20,
-                      color: Colors.teal[100],
-                      letterSpacing: 5,
-                      fontWeight: FontWeight.bold),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.mail,
+                    color: Colors.teal,
+                  ),
+                  title: Text(
+                    'abdo@gmail.com',
+                    style: TextStyle(
+                        fontFamily: 'Source Sans Pro',
+                        fontSize: 20,
+                        color: Colors.teal[600],
+                        fontWeight: FontWeight.bold),
+                  ),
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
         ),
       ),
